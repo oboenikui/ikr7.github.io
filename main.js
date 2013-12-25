748f52f866da9f0667ba45187e4f9129fe724a4b
ikura <ikura@ikura.local>
2013-12-25 15:02:42 +0900

    チンポ

--- main.js
+++ main.js
@@ -3,32 +3,56 @@ var cvs = document.querySelector('canvas');
 var ctx = cvs.getContext('2d');
 
 
-var drawikr7 = function(x, y){
+var drawikr7 = function(x, y, size){
 	var ikr7 = new Image();
 	ikr7.src = './ikr7.png';
 	ikr7.addEventListener('load', function(){
-		ctx.drawImage(ikr7, x - 20, y - 20, 40, 40);
+		ctx.drawImage(ikr7, x - size / 2, y - size / 2, size, size);
 	}, false);
 };
 
-var w = cvs.width;
-var h = cvs.height;
-
-var i = -1 * w / 2;
-var timer = setInterval(function(){
-	if(i < w / 2){
-		var x = i;
-		var y = -1 * f(x);
-		drawikr7(x + w / 2, y + h / 2);
-		i += 2;
-	}else{
-		clearInterval(timer);
-		console.log('End.');
-	}
-}, 10);
-
-function f(x){
-	var y;
-	y = x * x;
+var f = function(x){
+	y = Math.sin(x * Math.PI / 180) * 250;
 	return y;
-}
+};
+
+var applyFunc = function(){
+
+	eval(document.querySelector('textarea').value);
+
+	var w = cvs.width;
+	var h = cvs.height;
+
+	ctx.clearRect(0, 0, w, h);
+
+	ctx.beginPath();
+	ctx.moveTo(cvs.width / 2, 0);
+	ctx.lineTo(cvs.width / 2, cvs.height);
+	ctx.moveTo(0, cvs.height / 2);
+	ctx.lineTo(cvs.width, cvs.height / 2);
+	ctx.strokeStyle = '#AAA';
+	ctx.stroke();
+
+	ctx.font = '12px "Lucida Grande", Helvetica, Arial, sans-serif';
+	ctx.fillText('0', 252, 248);
+	ctx.fillText('X', 485, 248);
+	ctx.fillText('Y', 252, 15);
+
+	var i = -1 * w / 2;
+	var timer = setInterval(function(){
+		if(i < w / 2){
+			var x = i;
+			var y = -f(x);
+			drawikr7(x + w / 2, y + h / 2, 100);
+			i += 5;
+		}else{
+			clearInterval(timer);
+			//applyFunc();
+			console.log('End.');
+		}
+	}, 10);
+};
+
+document.querySelector('button').addEventListener('click', applyFunc, false);
+
+applyFunc();
